27f70bb65613a0669d9fd9fdb66e3203d11de41a
Sucheta Sikdar <suchetasikdar1@gmail.com>
2025-08-03 16:27:22 -0400

    Add files via upload

---
+++ NationalPark.cs
@@ -0,0 +1,10 @@
+﻿namespace CanadianNationalParks.Models
+{
+    public class NationalPark
+    {
+        public string Name { get; set; }
+        public string ProvinceOrTerritory { get; set; }
+        public string NotableAnimals { get; set; }
+    }
+}
+
