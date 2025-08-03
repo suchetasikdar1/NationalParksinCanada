6383c18374b60af1dc5812502365b92a26f2011c
Sucheta Sikdar <suchetasikdar1@gmail.com>
2025-08-03 16:37:07 -0400

    Create NationalPark.cs

---
+++ Models/NationalPark.cs
@@ -0,0 +1,9 @@
+namespace CanadianNationalParks.Models
+{
+    public class NationalPark
+    {
+        public string Name { get; set; }
+        public string ProvinceOrTerritory { get; set; }
+        public string NotableAnimals { get; set; }
+    }
+}
